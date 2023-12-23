4a3ed2071c84c9dfa6e33bf87af208dee2d8cb38
Jacob Graver <jacob.graver@gmail.com>
2023-12-23 13:12:25 -0700

    working on scrollable frame

--- main.py
+++ main.py
@@ -14,4 +14,23 @@ app.title("Recipie Database")
 title = customtkinter.CTkLabel(app, text="List of Recipes")
 title.pack(padx=10, pady=10)
 
+class MyFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        # add widgets onto the frame...
+        self.label = customtkinter.CTkLabel(self)
+        self.label.grid(row=0, column=0, padx=20)
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # self.my_frame = MyFrame(master=self, width=300, height=200)
+        # self.my_frame.grid(row=0, column=0, padx=20, pady=20)
+
+
+app = App()
+
 app.mainloop()
