d9e785e65b9a5b173f55141649a08373b63b0df1
Jacob Graver <jacob.graver@gmail.com>
2023-12-22 21:28:16 -0700

    initial app creation

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import tkinter
 import customtkinter
 
+# System settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# app frame
+app = customtkinter.CTk()
+app.geometry("1280x720")
+app.title("Recipie Database")
+
+# Adding UI elements
+title = customtkinter.CTkLabel(app, text="List of Recipes")
+title.pack(padx=10, pady=10)
+
+app.mainloop()
