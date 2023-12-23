1863c4344282215f471559d858b2b7f8f0fda719
Jacob Graver <jacob.graver@gmail.com>
2023-12-23 13:15:34 -0700

    scrollable frame added but not in correct location yet.

--- main.py
+++ main.py
@@ -14,23 +14,7 @@ app.title("Recipie Database")
 title = customtkinter.CTkLabel(app, text="List of Recipes")
 title.pack(padx=10, pady=10)
 
-class MyFrame(customtkinter.CTkScrollableFrame):
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
-
-        # add widgets onto the frame...
-        self.label = customtkinter.CTkLabel(self)
-        self.label.grid(row=0, column=0, padx=20)
-
-
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-
-        # self.my_frame = MyFrame(master=self, width=300, height=200)
-        # self.my_frame.grid(row=0, column=0, padx=20, pady=20)
-
-
-app = App()
+scrollable_frame = customtkinter.CTkScrollableFrame(app, width=400, height=350)
+scrollable_frame.pack(padx=10, pady=10)
 
 app.mainloop()
