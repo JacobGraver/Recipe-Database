5ad1df77d1b8a528cdf67180c91795238ee9745e
Jacob Graver <jacob.graver@gmail.com>
2024-02-19 15:39:43 -0700

    Going to experiment with breaking out functions into their own files

---
+++ modules/add_recipe.py
@@ -0,0 +1,71 @@
+import sqlite3
+
+def add_recipe():
+	
+    conn = sqlite3.connect('recipes.db')
+
+	#################################################################
+	######################### RECIPE NAME ###########################
+	#################################################################
+	
+	# Get the name of the recipe and create a table that is named after the recipe and has the recipe name in the table
+	
+    recipe_name = input(f"What is the name of the recipe?\n")
+	
+    conn.execute("CREATE TABLE *(RecipeName, PrepTime, CookTime, Ingredients, Instructions)", recipe_name)
+    conn.execute("INSERT INTO *(RecipeName) VALUES(recipe_name)")
+
+	#################################################################
+	########################## Prep Time ############################
+	#################################################################
+
+	# Get the prep time and put it into the table 
+    prep_time = input(f"How long does the recipe take to prep?\n")
+    conn.execute("INSERT INTO * (PrepTime) VALUES(*)", recipe_name, prep_time)
+
+	#################################################################
+	########################## Cook Time ############################
+	#################################################################
+
+    cook_time = input(f"How long does the recipe take to cook?\n")
+    conn.execute("INSERT INTO * (CookTime) VALUES(*)", recipe_name, cook_time)
+	
+	#################################################################
+	######################### Ingredients ###########################
+	#################################################################
+
+    ingredients = input(f"What is the first ingredient?\n")
+    conn.execute("INSERT INTO * (Ingredients) VALUES(*)", recipe_name, ingredients)
+
+	# Decision loop for adding more ingredients
+    while True:
+        choice = input(f"Would you like to add another ingredient? ('yes' or 'no')\n")
+        if choice == 'no'
+        	break
+
+    	ingredient = input(f"What is the ingredient?\n")
+        conn.execute("INSERT INTO * (Ingredients) VALUES(*)", recipe_name, ingredient)
+
+	#################################################################
+	######################### Instructions ##########################
+	#################################################################
+
+	# Adding the instructions to the db 
+	# Each step (*should) be in its own cell in the column so that is how they will be displayed
+	step_1 = input(f"What is the first step?\n")
+	conn.execute("INSERT INTO * (Instructions) VALUES (*)", recipe_name, step_1)
+
+	# Reseting the value for choice so it cant cause issues
+	choice = ' '
+
+	# Decision loop for the steps of the recipe 
+	while True:
+		choice = input(f"Would you like to add another step?('yes' or 'no')\n")
+		if choice == 'no'
+			break
+
+		next_step = input(f"What is the next step?\n")
+		conn.execute("INSERT INTO * (Instructions) VALUES (*)", recipe_name, next_step)
+
+
+add_recipe()
