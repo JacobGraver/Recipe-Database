03f8d2ac7272d1f84936dd53e038b3e9ec5854ef
Jacob Graver <jacob.graver@gmail.com>
2024-02-19 15:16:03 -0700

    Making really nice comments

--- main.py
+++ main.py
@@ -46,15 +46,34 @@ def cli_prompt():
 
 def add_recipe():
 
+	#################################################################
+	######################### RECIPE NAME ###########################
+	#################################################################
+	
 	# Get the name of the recipe and create a table that is named after the recipe and has the recipe name in the table
 	recipe_name = input(f"What is the name of the recipe?\n")
 	conn.execute("CREATE TABLE *(RecipeName, PrepTime, CookTime, Ingredients, Instructions)", recipe_name)
 	conn.execute("INSERT INTO *(RecipeName) VALUES(recipe_name)")
 
+	#################################################################
+	########################## Prep Time ############################
+	#################################################################
+
 	# Get the prep time and put it into the table 
 	prep_time = input(f"How long does the recipe take to prep?\n")
 	conn.execute("INSERT INTO * (PrepTime) VALUES(*)", recipe_name, prep_time)
+
+	#################################################################
+	########################## Cook Time ############################
+	#################################################################
+
+	cook_time = input(f"How long does the recipe take to cook?\n")
+	conn.execute("INSERT INTO * (CookTime) VALUES(*)", recipe_name, cook_time)
 	
+	#################################################################
+	######################### Ingredients ###########################
+	#################################################################
+
 	ingredients = input(f"What is the first ingredient?\n")
 	conn.execute("INSERT INTO * (Ingredients) VALUES(*)", recipe_name, ingredients)
 
@@ -65,6 +84,10 @@ def add_recipe():
 
 		ingredient = input(f"What is the ingredient?\n")
 		conn.execute("INSERT INTO * (Ingredients) VALUES(*)", recipe_name, ingredient)
+
+	#################################################################
+	######################### Instructions ##########################
+	#################################################################
 
 	# Adding the instructions to the db 
 	# Each step will be in its own cell in the column so that is how they will be displayed
@@ -78,6 +101,9 @@ def add_recipe():
 	while choice != 'no':
 		choice = ' '
 		choice = input(f"Would you like to add another step?('yes' or 'no')\n")
+
+		next_step = input(f"What is the next step?\n")
+		conn.execute("INSERT INTO * (Instructions) VALUES (*)", recipe_name, next_step)
 
 
 
