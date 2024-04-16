1dbd91f66595fec9b7d81ea821c7c7573f574bcf
Jacob Graver <jacob.graver@gmail.com>
2024-04-16 17:06:02 -0600

    small changes while testing emacs

--- update.py
+++ update.py
@@ -1,19 +1,33 @@
 import json
 import os
 
+
 def update_recipe():
     # Get the name of the recipe to be updated
-    name = input(print('What is the name of the recipe that you would like to update? '))
+    name = input(
+        print("What is the name of the recipe that you would like to update? ")
+    )
+
+    file_name = name + ".json"
+
+    # TODO write search function
+
+    # getting the key for the dict
+    recipe = input(print("What part of the recipe would you like to update?"))
+
+    with open(file_name, 'r') as f:
+        loaded_dict = json.load(f)
+
+    to_change = input(print('What part of the recipe would you like to change?'))
     
-    try:
-        # Search and make sure the recipe exists
-        # Might not work and might need to be updated 
-        # FIXME
-        os.path.isfile('/home/myth/Git/Recipe-Database/Recipes')
-        
-    except Exception as e:
-        print(f'Error: {e}')
-        
-    # Get the part of the recipe that needs updated
-    change = input(print('What part of the recipe would you like to update?'))
+    change = input(print('Please type the changed verion now'))
     
+    loaded_dict[to_change] = change
+
+    # load the changed dict into a json string
+    json_str = json.dumps(loaded_dict)
+    
+    # load the json string into a file 
+    with open(file_name, 'w') as f:
+        loaded_dict = json.load(f)
+
